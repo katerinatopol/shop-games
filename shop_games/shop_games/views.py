ae6ac927feca8eb933d2383e77af4240983bec1f
katerina_topol <katerina_topol@mail.ru>
2021-06-29 22:35:00 +0300

    add base.html, includes in shop_games

--- shop_games/shop_games/views.py
+++ shop_games/shop_games/views.py
@@ -2,24 +2,24 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'shop_games/index.html')
 
 
 def contacts(request):
-    return render(request, 'contact.html')
+    return render(request, 'shop_games/contact.html')
 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'shop_games/about.html')
 
 
 def services(request):
-    return render(request, 'services.html')
+    return render(request, 'shop_games/services.html')
 
 
 def news(request):
-    return render(request, 'news.html')
+    return render(request, 'shop_games/news.html')
 
 
 def team(request):
-    return render(request, 'team.html')
+    return render(request, 'shop_games/team.html')
