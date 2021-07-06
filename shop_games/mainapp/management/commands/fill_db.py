77babd493802d643a64903a69262657bdbfae0e9
katerina_topol <katerina_topol@mail.ru>
2021-07-06 19:27:46 +0300

    add json

---
+++ shop_games/mainapp/management/commands/fill_db.py
@@ -0,0 +1,47 @@
+import os
+import json
+
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+from shop_games.mainapp.models import GamesCategory, Games, Image
+
+JSON_PATH = 'mainapp/jsons'
+
+
+def load_from_json(file_name):
+    with open(os.path.join(JSON_PATH, file_name + '.json'), mode='r', encoding='UTF-8')as infile:
+
+        return json.load(infile)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        categories = load_from_json('categories')
+
+        GamesCategory.objects.all().delete()
+        for category in categories:
+            new_category = GamesCategory(**category)
+            new_category.save()
+
+        games = load_from_json('games')
+
+        Games.objects.all().delete()
+        for game in games:
+            category_name = game['category']
+            _category = GamesCategory.objects.get(name=category_name)
+            game['category'] = _category
+            new_category = Games(**game)
+            new_category.save()
+
+        images = load_from_json('images')
+
+        Image.objects.all().delete()
+        for image in images:
+            image_game = image['game']
+            _game = Games.objects.get(name=image_game)
+            image['game'] = _game
+            new_category = Games(**game)
+            new_category.save()
+
+        super_user = User.objects.create_superuser('admin', 'admin@geekshop.local', '123')
