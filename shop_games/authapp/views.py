85083bd610f55672a561d5e871300d560589184b
Ekaterina Topol <katerina_topol@mail.ru>
2021-07-28 17:03:00 +0300

    Update views.py

--- shop_games/authapp/views.py
+++ shop_games/authapp/views.py
@@ -2,10 +2,13 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib import auth
 from django.urls import reverse
+
+from authapp.models import ShopUser
 from shop_games import settings
 from django.core.mail import send_mail
 
 from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
+
 
 def send_verify_email(user):
     verify_link = reverse('auth:verify', args=[user.email, user.activation_key])
@@ -16,6 +19,22 @@ def send_verify_email(user):
               f' перейдите по ссылке: \n{settings.DOMAIN_NAME}{verify_link}'
 
     return send_mail(title, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
+
+
+def verify(request, email, activation_key):
+    try:
+        user = ShopUser.objects.get(email=email)
+        if user.activation_key == activation_key and not user.is_activation_key_expired():
+            user.is_active = True
+            user.save()
+            auth.login(request, user)
+            return render(request, 'authapp/verification.html')
+        else:
+            print(f'error activation user {user.username}')
+            return render(request, 'authapp/verification.html')
+    except Exception as err:
+        print(f'error activation user: {err.args}')
+        return HttpResponseRedirect(reverse('index'))
 
 
 def login(request):
@@ -57,8 +76,13 @@ def register(request):
         register_form = ShopUserRegisterForm(request.POST, request.FILES)
 
         if register_form.is_valid():
-            register_form.save()
-            return HttpResponseRedirect(reverse('auth:login'))
+            user = register_form.save()
+            if send_verify_email(user):
+                print('Сообщение отправлено.')
+                return HttpResponseRedirect(reverse('auth:login'))
+            else:
+                print('Неудлаось отправить сообщение.')
+                return HttpResponseRedirect(reverse('auth:login'))
     else:
         register_form = ShopUserRegisterForm()
 
